53a93a39678e34516ee8042f1d13f2e96f0c0218
Abdallah <bratosab@gmail.com>
2022-06-20 21:27:01 +0200

    feat(click travel): Initialised Boiler blate for click travel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'click-travel-ng14';
+  title = 'Choose your dream destination...';
 }
