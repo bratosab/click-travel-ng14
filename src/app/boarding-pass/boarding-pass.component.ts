b3fc39198cc1bac33f9ee94aa13f803c2c877ced
Abdallah <bratosab@gmail.com>
2022-06-22 15:01:27 +0200

    partie 4

--- src/app/boarding-pass/boarding-pass.component.ts
+++ src/app/boarding-pass/boarding-pass.component.ts
@@ -1,18 +1,43 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Ticket } from '../models/tickets.interface';
 
 @Component({
   selector: 'app-boarding-pass',
   templateUrl: './boarding-pass.component.html',
-  styleUrls: ['./boarding-pass.component.scss']
+  styleUrls: ['./boarding-pass.component.scss'],
 })
-export class BoardingPassComponent implements OnInit {
+export class BoardingPassComponent implements OnChanges {
+  boarding: string;
+  departure: string;
+  duration: string;
+  arrival: string;
 
-  @Input() ticket: Ticket;
-
-  constructor() { }
-
-  ngOnInit(): void {
+  private _ticket: Ticket;
+  @Input() 
+  set ticket(value: Ticket) {
+    this._ticket = value;
+    this.calculateTimes();
+  }
+  get ticket(): Ticket {
+    return this._ticket;
   }
 
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('changes', changes)
+  }
+
+
+  private calculateTimes() {
+    console.log('calculateTimes')
+    const departureDate = new Date(this.ticket.time);
+    this.boarding = '';
+    this.departure = departureDate.toLocaleString('fr-FR', {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+    this.duration = '';
+    this.arrival = '';
+  }
 }
