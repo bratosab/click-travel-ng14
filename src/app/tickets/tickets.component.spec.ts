36f3cf9bfb880668fc668d8ca06ac6691b4c35b6
Abdallah <bratosab@gmail.com>
2023-03-02 14:59:29 +0100

    feat(tests): Adde component testing

--- src/app/tickets/tickets.component.spec.ts
+++ src/app/tickets/tickets.component.spec.ts
@@ -1,14 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { ClickTravelService } from '../click-travel.service';
+import destinationsMock from '../mocks/destinations.mock';
+import ticketsMock from '../mocks/tickets.mock';
 
 import { TicketsComponent } from './tickets.component';
 
 describe('TicketsComponent', () => {
   let component: TicketsComponent;
   let fixture: ComponentFixture<TicketsComponent>;
+  let clickTravelServiceStub: Partial<ClickTravelService>;
 
   beforeEach(async () => {
+    clickTravelServiceStub = {
+      getTickets: () => {
+        return of(ticketsMock);
+      },
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ TicketsComponent ]
+      declarations: [ TicketsComponent ],
+      imports: [RouterTestingModule],
+      providers: [
+        { provide: ClickTravelService, useValue: clickTravelServiceStub },
+      ],
     })
     .compileComponents();
 
@@ -17,7 +33,7 @@ describe('TicketsComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
